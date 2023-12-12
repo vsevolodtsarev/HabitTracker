8ce3dc9a93534a6b51bb79fe39d8dc35e6a72eac
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2023-12-12 13:49:43 +0300

    Add TrackerCreatingView

---
+++ HabitTracker/Views/TrackerCreatingView.swift
@@ -0,0 +1,18 @@
+//
+//  TrackerCreatingView.swift
+//  HabitTracker
+//
+//  Created by Всеволод Царев on 12.12.2023.
+//
+
+import SwiftUI
+
+struct TrackerCreatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrackerCreatingView()
+}
