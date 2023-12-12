7b39cfd23b456580060dfd38eb1e9270aa756153
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2023-12-12 19:05:18 +0300

    Add ColorView

---
+++ HabitTracker/Views/Components/ColorView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorsView.swift
+//  HabitTracker
+//
+//  Created by Всеволод Царев on 12.12.2023.
+//
+
+import SwiftUI
+
+struct ColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorsView()
+}
