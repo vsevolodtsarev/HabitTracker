68f9c6e3651baf6b87e3135490230e802d3cbcb1
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-24 10:02:18 +0300

    Update ordered days

--- HabitTracker/Models/Tracker.swift
+++ HabitTracker/Models/Tracker.swift
@@ -65,13 +65,13 @@ enum Schedule: String, CaseIterable, Codable {
     
     var orderDay: Int {
         switch self {
-        case .monday: return 2
-        case .tuesday: return 3
-        case .wednesday: return 4
-        case .thursday: return 5
-        case .friday: return 6
-        case .saturday: return 7
-        case .sunday: return 1
+        case .monday: return 1
+        case .tuesday: return 2
+        case .wednesday: return 3
+        case .thursday: return 4
+        case .friday: return 5
+        case .saturday: return 6
+        case .sunday: return 7
         }
     }
 }
