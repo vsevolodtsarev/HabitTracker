34e1b98057a6447133f978afb3f756fcd671d2d5
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-24 09:57:25 +0300

    Add orderDay to Schedule

--- HabitTracker/Models/Tracker.swift
+++ HabitTracker/Models/Tracker.swift
@@ -62,6 +62,18 @@ enum Schedule: String, CaseIterable, Codable {
         case .sunday: return "Вс"
         }
     }
+    
+    var orderDay: Int {
+        switch self {
+        case .monday: return 2
+        case .tuesday: return 3
+        case .wednesday: return 4
+        case .thursday: return 5
+        case .friday: return 6
+        case .saturday: return 7
+        case .sunday: return 1
+        }
+    }
 }
 
 enum TypeOfTracker {
