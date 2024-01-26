cf3ae607ab1ff299f5523eea2f7545b4441e5b79
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-26 09:48:36 +0300

    Add OnboardingView

---
+++ HabitTracker/Views/OnboardingView.swift
@@ -0,0 +1,34 @@
+//
+//  OnboardingView.swift
+//  HabitTracker
+//
+//  Created by Всеволод Царев on 26.01.2024.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    @State var currentIndex: Int = 0
+    
+    var body: some View {
+        TabView(selection: $currentIndex) {
+            OnboardingScreen(image: .onboarding1, subscription: "Отслеживайте только то, что хотите")
+                .ignoresSafeArea()
+                .tag(0)
+            
+            OnboardingScreen(image: .onboarding2, subscription: "Даже если это \n не литры воды и йога")
+                .ignoresSafeArea()
+                .tag(1)
+            
+        }
+        .ignoresSafeArea()
+        .tabViewStyle(.page(indexDisplayMode: .always))
+     
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
+
+
