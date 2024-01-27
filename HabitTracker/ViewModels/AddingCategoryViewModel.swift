b1fd27400e1910a18adf0969df6f1591d531b485
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-27 05:46:10 +0300

    Add AddingCategoryView + ViewModel

---
+++ HabitTracker/ViewModels/AddingCategoryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddingCategoryViewModel.swift
+//  HabitTracker
+//
+//  Created by Всеволод Царев on 27.01.2024.
+//
+
+import Foundation
---
+++ HabitTracker/Views/AddingCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  AddingCategoryView.swift
+//  HabitTracker
+//
+//  Created by Всеволод Царев on 27.01.2024.
+//
+
+import SwiftUI
+
+struct AddingCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingCategoryView()
+}
