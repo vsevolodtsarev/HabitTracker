39d981ebe7f74aff4e289bd37e97d5bf2ada3ec7
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2024-01-28 06:44:12 +0300

    Add StatisticsViewModel

--- HabitTracker/Views/StatiscticsView.swift
+++ HabitTracker/Views/StatiscticsView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct StatiscticsView: View {
+    private let statisticsViewModel = StatisticsViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
